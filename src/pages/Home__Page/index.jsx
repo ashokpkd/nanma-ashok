06215525316d26aa2e652200eb03255eecc9dad5
ashok <ashokkumar.a1999@gmail.com>
2024-02-06 18:21:56 +0530

    custom hooks changes added

--- src/pages/Home__Page/index.jsx
+++ src/pages/Home__Page/index.jsx
@@ -1,16 +1,7 @@
-import React from 'react'
-import useCustomApi from '../../hooks/useCustomApi'
+import React from "react";
 
 const HomePage = () => {
-  const { get, post, error, data } = useCustomApi()
-  
-  get('https://fakestoreapi.com/products')
-  console.log('fetch',data,error)
-  return (
-    <div>
-home page
-    </div>
-  )
-}
+  return <div>home page</div>;
+};
 
-export default HomePage
+export default HomePage;
